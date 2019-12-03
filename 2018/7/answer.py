11e9133139232b782cbc18bf4f0d8dc08d4a88a5
James Cockbain <james.cockbain@ibm.com>
2019-12-03 21:03:48 +0000

    add 2019 day 3 solution

--- 2018/7/answer.py
+++ 2018/7/answer.py
@@ -10,12 +10,7 @@ for line in input:
 
 
 def part_1(rules):
-    tasks = set()
-    deps = defaultdict(set)
-    for rule in rules:
-        a, b = rule
-        tasks |= {a, b}
-        deps[b].add(a)
+    tasks, deps = get_tasks_and_deps(rules)
     done = []
     for _ in tasks:
         done.append(
@@ -23,8 +18,41 @@ def part_1(rules):
     return ''.join(done)
 
 
-def part_2(points):
-    pass
+def part_2(rules):
+    tasks, deps = get_tasks_and_deps(rules)
+    done = set()
+    seconds = 0
+    counts = [0] * 5
+    work = [''] * 5
+    while True:
+        for i, count in enumerate(counts):
+            if count == 1:
+                done.add(work[i])
+            counts[i] = max(0, count - 1)
+        while 0 in counts:
+            i = counts.index(0)
+            candidates = [x for x in tasks if deps[x] <= done]
+            if not candidates:
+                break
+            task = min(candidates)
+            tasks.remove(task)
+            counts[i] = ord(task) - ord('A') + 61
+            work[i] = task
+        if sum(counts) == 0:
+            break
+        seconds += 1
+    return seconds
+
+
+def get_tasks_and_deps(rules):
+    tasks = set()
+    deps = defaultdict(set)
+    for rule in rules:
+        a, b = rule
+        tasks |= {a, b}
+        deps[b].add(a)
+    return tasks, deps
 
 
 print(part_1(input_list))
+print(part_2(input_list))
