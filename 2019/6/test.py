3c8fbfb9349a0c64d93d070d6534e84ebfe0386c
James Cockbain <james.cockbain@ibm.com>
2019-12-06 08:17:22 +0000

    add test for 2019 day 6 part 2

--- 2019/6/test.py
+++ 2019/6/test.py
@@ -5,10 +5,20 @@ import answer
 
 class TestSum(unittest.TestCase):
     def test_part_1(self):
-        data = [["COM", "B"], ["B", "C"], ["C", "D"], ["D", "E"], ["E", "F"],
-                ["B", "G"], ["G", "H"], ["D", "I"], ["E", "J"], ["J", "K"], ["K", "L"]]
+        data = [["COM", "B"], ["B", "C"], ["C", "D"],
+                ["D", "E"], ["E", "F"], ["B", "G"],
+                ["G", "H"], ["D", "I"], ["E", "J"],
+                ["J", "K"], ["K", "L"]]
         result = answer.part_1(data)
         self.assertEqual(result, 42)
+
+    def test_part_2(self):
+        data = [["COM", "B"], ["B", "C"], ["C", "D"],
+                ["D", "E"], ["E", "F"], ["B", "G"], ["G", "H"],
+                ["D", "I"], ["E", "J"], ["J", "K"],
+                ["K", "L"], ["K", "YOU"], ["I", "SAN"]]
+        result = answer.part_2(data)
+        self.assertEqual(result, 4)
 
 
 if __name__ == '__main__':
