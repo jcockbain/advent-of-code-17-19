e0f021b363a2a6f9dfb12f3226d1fa8d6da08f7b
James Cockbain <james.cockbain@ibm.com>
2019-12-08 17:56:42 +0000

    refactor 2017 day 4

--- 2017/4/answer.py
+++ 2017/4/answer.py
@@ -1,37 +1,20 @@
-from collections import Counter
-
-
-def is_anagram(a, b):
-    return Counter(a) == Counter(b)
-
-
-def p1(passwords):
+def p1(words):
     count = 0
-    for p in passwords:
-        words = p.split(' ')
-        word_count = Counter(words)
-        if word_count.most_common(1)[0][1] <= 1:
+    for p in words:
+        if len(p) == len(set(p)):
             count += 1
     return count
 
 
-def p2(passwords):
+def p2(words):
     p2 = 0
-    for p in passwords:
-        words = p.split(' ')
-        anagram_found = False
-        for i in range(len(words)):
-            for j in range(i+1, len(words)):
-                if is_anagram(words[i], words[j]):
-                    anagram_found = True
-        if not anagram_found:
+    for p in words:
+        if len(set(''.join(sorted(word)) for word in p)) == len(p):
             p2 += 1
     return p2
 
 
 if __name__ == '__main__':
-    with open('input.in') as f:
-        input = f.read()
-    passwords = input.split('\n')
+    passwords = [i.strip().split() for i in open('input.in', 'r').readlines()]
     print('part 1: ', p1(passwords))
     print('part 2: ', p2(passwords))
